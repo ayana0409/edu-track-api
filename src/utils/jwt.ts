21ab41ca47311eede1038e70c6ac8ffec386b400
ayana0409 <duongdoanthuan2003@gmail.com>
2025-12-28 18:21:42 +0700

    add new func

--- src/utils/jwt.ts
+++ src/utils/jwt.ts
@@ -1,6 +1,7 @@
-import jwt, { SignOptions } from "jsonwebtoken";
+import jwt, { JwtPayload, SignOptions } from "jsonwebtoken";
 import { Types } from "mongoose";
 import { env } from "../configs/env";
+import { ApiError } from "./api-error";
 
 const accessTokenOptions: SignOptions = {
   expiresIn: env.JWT_ACCESS_EXPIRES,
@@ -10,13 +11,22 @@ const refreshTokenOptions: SignOptions = {
   expiresIn: env.JWT_REFRESH_EXPIRES,
 };
 
+export interface AccessTokenPayload extends JwtPayload {
+  userId: string;
+  role: string;
+}
+
+export interface RefreshTokenPayload extends JwtPayload {
+  userId: string;
+}
+
 export const signAccessToken = (
   userId: Types.ObjectId,
   role: string
 ): string => {
   return jwt.sign(
     { userId: userId.toString(), role },
-    process.env.JWT_ACCESS_SECRET as string,
+    env.JWT_ACCESS_SECRET,
     accessTokenOptions
   );
 };
@@ -24,7 +34,26 @@ export const signAccessToken = (
 export const signRefreshToken = (userId: Types.ObjectId): string => {
   return jwt.sign(
     { userId: userId.toString() },
-    process.env.JWT_REFRESH_SECRET as string,
+    env.JWT_REFRESH_SECRET,
     refreshTokenOptions
   );
+};
+
+export const verifyAccessToken = (token: string): AccessTokenPayload => {
+  try {
+    return jwt.verify(token, env.JWT_ACCESS_SECRET) as AccessTokenPayload;
+  } catch {
+    throw new ApiError(401, "Invalid or expired access token");
+  }
+};
+
+/**
+ * Verify refresh token
+ */
+export const verifyRefreshToken = (token: string): RefreshTokenPayload => {
+  try {
+    return jwt.verify(token, env.JWT_REFRESH_SECRET) as RefreshTokenPayload;
+  } catch {
+    throw new ApiError(401, "Invalid or expired refresh token");
+  }
 };
